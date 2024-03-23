c8239186ee63da62eec0818a82effe39ac7db75d
Rachel Huang <rachelhuang505@gmail.com>
2024-03-23 16:47:45 -0400

    added components to home page and created login page with username

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react"; // useState for storing data and useEffect for changing data on click 
 import * as React from 'react';
+import ReactPaginate from "react-paginate"; // for pagination
+import styled from "styled-components";
+import { Router, Route, Routes, useNavigate } from "react-router-dom";
+import { RequireAuth } from "react-auth-kit";
+import ReactDOM from 'react-dom/client';
+import { saveAs } from 'file-saver';
+
 //can read .csv file data and display it on the page: if we write data that we collect from the api to a csv
 //file, we will be able to use it in react 
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 
 function TickerDisplay(){
   return(<>
   <div className = "tickerContainer">
-  <img src = "./testgraph.png" className = "images"/>
-    <button>Buy</button>
-    <button>Sell</button></div></>
+  <img src = "./AppleFirstTest.png" className = "images"/>
+    <button className = "buyButton">Buy</button>
+    <button className = "sellButton">Sell</button></div></>
     );
 }
 
 function NavBar(){
   return(<>
-  <ul>
+  <ul className = "header">
   <li><a href="default.asp">Home</a></li>
   <li><a href="news.asp">News</a></li>
   <li><a href="contact.asp">Contact</a></li>
@@ -24,7 +37,11 @@ function NavBar(){
 </ul></>);
 }
 
-function App() {
+function Control(props) {
+  const [page, setPage] = useState(0);
+  const [filterData, setFilterData] = useState();
+  const n = 1;
+
   const [ text, setText ] = useState();
 
     fetch( './constituents.csv' )
@@ -33,9 +50,80 @@ function App() {
             setText(responseText.slice(0, 100));
         })
 
+        const rows = [];
+        for (let i = 0; i < 500; i++) {
+            rows.push(<TickerDisplay />);
+        }
+
+        useEffect(() => {
+          setFilterData(
+            rows.filter((item, index) => {
+              return (index >= page * n) & (index < (page + 1) * n);
+            })
+          );
+        }, [page]);
+  const [displayLogin, setLogin] = useState(props.displayLogin);
+  const [name, setName] = useState("");
+  const handleSubmit = (event) => {
+    setLogin(false);
+  }
+  //scuffed way of redirecting after login (toggles login stuff)
+  if (displayLogin) {
+    return(
+    <form className = "loginPage" onSubmit={handleSubmit}>
+      <div><label>Enter your name:</label></div>
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => {setName(e.target.value);
+          }}
+        />
+      <div><input className = "inputField" type="submit" /></div>
+    </form>);
+  }
+  
+  return(<><NavBar/><div class="container">
+  <div class="column">
+    <h2> {name}'s current stats </h2>
+  <p> money</p>
+  <p> stocks they hold</p>
+  </div>
+  <div class="column" style={{ height: '100vh', overflow: 'scroll' }}>
+  <h2> stock visualizations </h2>
+  <ul>
+{filterData && filterData.map((item, index) => <li>Stock Ticker:{item}</li>)}
+</ul>
+<ReactPaginate
+  containerClassName={"pagination"}
+  pageClassName={"page-item"}
+  activeClassName={"active"}
+  className = "graphs"
+  onPageChange={(event) => setPage(event.selected)}
+  pageCount={Math.ceil(rows.length / n)}
+  breakLabel="..."
+  previousLabel={
+    "<"
+  }
+  nextLabel={
+    ">"
+  }
+/>
+
+
+  </div>
+  <div class="column">
+    <h2> other info </h2>
+    <pre>{ text }</pre>
+  </div>
+</div> </>);
+}
+
+function App() {
+
   return (
     <>
-    <NavBar/>
+    <Control displayLogin={true} />
+    {/* <NavBar/>
     <div class="container">
   <div class="column">
     <h2> User's current stats </h2>
@@ -43,17 +131,33 @@ function App() {
   <p> stocks they hold</p>
   </div>
   <div class="column" style={{ height: '100vh', overflow: 'scroll' }}>
-    <h2> visualizations for stocks </h2>
-    <TickerDisplay/>
-    <TickerDisplay/>
-    <TickerDisplay/>
+  <h2> stock visualizations </h2>
+  <ul>
+{filterData && filterData.map((item, index) => <li>Stock Ticker:{item}</li>)}
+</ul>
+<ReactPaginate
+  containerClassName={"pagination"}
+  pageClassName={"page-item"}
+  activeClassName={"active"}
+  className = "graphs"
+  onPageChange={(event) => setPage(event.selected)}
+  pageCount={Math.ceil(rows.length / n)}
+  breakLabel="..."
+  previousLabel={
+    "<"
+  }
+  nextLabel={
+    ">"
+  }
+/>
+
 
   </div>
   <div class="column">
     <h2> other info </h2>
     <pre>{ text }</pre>
   </div>
-</div>
+</div> */}
   </>
   );
 }
