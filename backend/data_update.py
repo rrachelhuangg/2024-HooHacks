e485d18cf9c9f81a27a2d0e64d2574beb7dd7249
jtaylor05 <jacksontay666@gmail.com>
2024-03-23 21:28:22 -0400

    edited data_update data format

--- backend/data_update.py
+++ backend/data_update.py
@@ -3,12 +3,13 @@ import numpy as np
 from datetime import *
 import os
 from os.path import expanduser
+import csv
 
 from polygon import RESTClient
 client = RESTClient(api_key="h8IoGYiQLCNjfJfU7je02JpmLrk4SJF9")
 
 home = os.path.expanduser("~")
-graph_data_path = os.path.join(home, "2024-HooHacks", "resources", "graph_data.csv")
+graph_data_path = os.path.join(home, "2024-HooHacks", "resources", "")
 
 
 def get_data(ticker, timespan, multiplier, start_date, end_date):
@@ -25,13 +26,18 @@ def get_data(ticker, timespan, multiplier, start_date, end_date):
     return aggs
 
 
-def data_runner(graph_data, timespan, multi, days_ago):
+def data_runner(graph_data_path, timespan, multi, days_ago):
     loop_running = True
-    tickers = graph_data['ticker']
+    string_data = pd.read_csv(graph_data_path + "tickers.csv")
+    tickers = string_data.ticker
     ticker_index = 0
     while(loop_running):
         current_ticker = tickers[ticker_index]
-        aggs, open, closed, highs, lows, time_index = (([] for i in range(0, 6)))
+        print(current_ticker)
+        path = graph_data_path + current_ticker + ".csv"
+        print(path)
+
+        aggs, opened, closed, highs, lows, time_index = (([] for i in range(0, 6)))
 
         end_date = datetime.now().strftime('%Y-%m-%d') #makes string of today
         start_date = (datetime.now() - timedelta(days_ago)).strftime('%Y-%m-%d') #makes string of starting day
@@ -41,29 +47,29 @@ def data_runner(graph_data, timespan, multi, days_ago):
         for i, a in enumerate(aggs):
             highs += [a.high]
             lows += [a.low]
-            open += [a.open]
+            opened += [a.open]
             closed += [a.close]
             time_index += [i]
 
         print(highs)
-
+        graph_data = {}
         try:
-            graph_data.loc[ticker_index] = {'GICS Sector': graph_data.loc[ticker_index]["GICS Sector"], 'ticker': current_ticker, 'open': np.array(open), 'closed': np.array(closed), 'high': np.array(highs), 'low': np.array(lows), 
-                                            'index': np.array(time_index), 'time_span': timespan, 'time_value': multi,
-                                            'start_date': start_date}
+            graph_data = pd.DataFrame({'open': opened, 'closed': closed, 'high': highs, 'low': lows, 
+                                            'index': time_index})
             print("set ticker " + current_ticker)
         except Exception as e:
             print("error in data setting: {}".format(e))
 
-        print(graph_data.loc[ticker_index])
-
-        graph_data.to_csv(graph_data_path, index=False)
+        if not os.path.isfile(path):
+            with open(path, mode="w") as f:
+                pass
+        
+        graph_data.to_csv(path, index=False)
 
         ticker_index = ticker_index + 1
-        if ticker_index > len(graph_data.index):
+        if ticker_index > len(string_data.index):
             ticker_index = 0
         #if ticker_index > 2: #remove this line to get rid of limitations
         #    loop_running = False
-
-graph_data = pd.read_csv(graph_data_path, dtype=object)
-data_runner(graph_data, "hour", "1", 1)
+            
+data_runner(graph_data_path, "hour", "1", 1)
